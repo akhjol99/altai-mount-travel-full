1760722d3f516efcb3b06bdf4bbe849d99ac5b43
Akhjol <macuser@Akhjols-MacBook-Pro.local>
2025-11-23 19:30:49 +0700

    Fix client-side error and head setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,11 @@ import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="https://altaimount.com/" />
-        <html lang="en" />
-      </Head>
+ 
     <main className="stack">
       <Component {...pageProps} />
     </main>
-    </>
+  
     
   );
 }

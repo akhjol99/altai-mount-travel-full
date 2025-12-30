d84b85dea7a6dc85f45c36047c5a74f3518a13b4
Akhjol <macuser@Akhjols-MacBook-Pro.local>
2025-12-30 17:07:27 +0700

    Web

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] }
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "altaimount.com" }],
+        destination: "https://www.altaimount.com/:path*",
+        permanent: true, // <- this makes it a 301
+      },
+    ];
+  },
 };
+
 module.exports = nextConfig;
